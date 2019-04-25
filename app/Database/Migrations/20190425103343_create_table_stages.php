9e3ddbeb51106ce463c2dce8042b12cb57f56841
msg8@duke.edu <matt.gatner@duke.ede>
2019-04-25 14:15:24 -0400

    Add more stages to seeder

--- app/Database/Migrations/20190425103343_create_table_stages.php
+++ app/Database/Migrations/20190425103343_create_table_stages.php
@@ -9,8 +9,9 @@ class Migration_create_table_stages extends Migration
 		$fields = [
 			'type'           => ['type' => 'varchar', 'constraint' => 31],
 			'name'           => ['type' => 'varchar', 'constraint' => 31],
-			'party'          => ['type' => 'varchar', 'constraint' => 31],
 			'uid'            => ['type' => 'varchar', 'constraint' => 31],
+			'route'          => ['type' => 'varchar', 'constraint' => 31],
+			'party'          => ['type' => 'varchar', 'constraint' => 31],
 			'icon'           => ['type' => 'varchar', 'constraint' => 31],
 			'summary'        => ['type' => 'varchar', 'constraint' => 255],
 			'description'    => ['type' => 'text'],
