9c3eccd392f23bdb93f796187d0da7bad53574e4
Matthew Gatner <matt.gatner@duke.edu>
2019-09-26 15:02:49 -0400

    Update Auth config for module changes

--- app/Config/Auth.php
+++ app/Config/Auth.php
@@ -67,7 +67,37 @@ class Auth extends \Myth\Auth\Config\Auth
     //--------------------------------------------------------------------
     // If true, will continue instead of throwing exceptions.
     //
-    public $silent = true;
+    public $silent = false;
+
+    /*--------------------------------------------------------------------
+	 * Encryption Algorithm to use
+	 *--------------------------------------------------------------------
+	 * Valid values are
+	 * - PASSWORD_DEFAULT (default)
+	 * - PASSWORD_BCRYPT
+	 * - PASSWORD_ARGON2I  - As of PHP 7.2 only if compiled with support for it
+	 * - PASSWORD_ARGON2ID - As of PHP 7.3 only if compiled with support for it
+	 *
+	 * If you choose to use any ARGON algorithm, then you might want to
+	 * uncomment the "ARGON2i/D Algorithm" options to suit your needs
+     */
+	public $hashAlgorithm = PASSWORD_DEFAULT;
+
+	/*--------------------------------------------------------------------
+	 * ARGON2i/D Algorithm options
+	 *--------------------------------------------------------------------
+     * The ARGON2I method of encryption allows you to define the "memory_cost",
+     * the "time_cost" and the number of "threads", whenever a password hash is
+     * created.
+     * This defaults to a value of 10 which is an acceptable number.
+     * However, depending on the security needs of your application
+     * and the power of your hardware, you might want to increase the
+     * cost. This makes the hashing process takes longer.
+     */
+
+	public $hashMemoryCost = 2048; 	//PASSWORD_ARGON2_DEFAULT_MEMORY_COST;
+	public $hashTimeCost = 4; 		//PASSWORD_ARGON2_DEFAULT_TIME_COST;
+	public $hashThreads = 4; 		//PASSWORD_ARGON2_DEFAULT_THREADS;
 
     //--------------------------------------------------------------------
     // Password Hashing Cost
@@ -80,7 +110,7 @@ class Auth extends \Myth\Auth\Config\Auth
     // cost. This makes the hashing process takes longer.
     //
     // Valid range is between 4 - 31.
-    public $hashCost = 25;
+    public $hashCost = 10;
 
     //--------------------------------------------------------------------
     // Minimum Password Length
@@ -101,6 +131,14 @@ class Auth extends \Myth\Auth\Config\Auth
     public $passwordValidators = [
         'Myth\Auth\Authentication\Passwords\CompositionValidator',
         'Myth\Auth\Authentication\Passwords\DictionaryValidator',
+        //'Myth\Auth\Authentication\Passwords\PwnedValidator',
     ];
 
+    //--------------------------------------------------------------------
+    // Reset Time
+    //--------------------------------------------------------------------
+    // The amount of time that a password reset-token is valid for,
+    // in seconds.
+    //
+    public $resetTime = 3600;
 }
