06adb11f1328460f338485a8490d800a84223766
Matt Gatner <matt.gatner@duke.edu>
2019-12-06 10:34:26 -0500

    Add example page contents

---
+++ app/Views/_examples/Privacy.php
@@ -0,0 +1,27 @@
+<h2>Privacy</h2>
+
+<h3>Visibility</h3>
+<section>
+	<p>
+		We print in an open lab and cannot guarantee printing that is completely obscured
+		from view. By request we can make extra efforts not to position your job in a way
+		that it can be seen by others, but our staff are not responsible for accidental
+		disclosures.
+	<p>
+</seciton>
+
+<h3>Sensitive jobs</h3>
+<section>
+	<p>
+		PHI and other jobs with sensitive information should be discussed with our staff
+		before submission.
+	</p>
+</section>
+
+<h3>Other policies</h3>
+<section>
+	<p>
+		This website and other services rendered adhere to the privacy policy of our
+		parent organization. Please review those policies before using our services.
+	</p>
+</section>
---
+++ app/Views/_examples/TOS.php
@@ -0,0 +1,46 @@
+<h2>Terms of Service</h2>
+
+<h3>Services</h3>
+<section>
+	<p>
+		Our staff will work diligently to complete your print job in a timely manner.
+		Printer and material availability, job complexity, and priority work all contribute
+		to possible delays in service so we cannot guarantee a delivery time. However we
+		will communicate regularly and offer additional status updates upon request.
+	</p>
+	<p>
+		3D printing is an experimental technology that often fails. Equipment failures are
+		not unusual. Our staff will make every attempt to print your object successfully.
+		If equipment breaks or the print failed without cause we will attempt to print it
+		again. The likelihood of failure increases significantly with more difficult-to-print
+		models. If the failure is a result of simply submitting too difficult of a model,
+		customers will still be charged an attempt fee of half the price of the initial
+		quote. Users can accept the failed job or we can make a second attempt at the print;
+		in this scenario, we will make additional attempts at 50% of the original estimate.
+	</p>
+</section>
+
+<h3>Pricing</h3>
+<section>
+	<p>
+		Print charges are estimated based on the material they will use and the time it
+		takes for staff to process them. Actual costs of completed jobs may vary from
+		from the issued estimate, and you are responsible for final cost within a reasonable
+		amount increase.
+	</p>
+	<p>
+		The cost of 3D printing varies widely and is based on the technology and amount of 
+		material used. Material prices are liable to change frequently and thus our printing
+		costs may also change without notice for new jobs.
+	</p>
+</section>
+
+<h3>Contract of agreement</h3>
+<section>
+	<ul>
+		<li>I agree that these terms are reasonable and I will abide by them in order ot use this service.</li>
+		<li>I understand charges vary between jobs, and actual costs may change during processing.</li>
+		<li>I understand very small parts increase likelihood of job failure</li>
+		<li>I confirm all model units are in millimeters</li>
+	</ul>
+</section>
