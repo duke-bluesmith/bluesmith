162bc5eba60684c2c58dd924ef92aa05c550a2e7
Matt Gatner <matt.gatner@duke.edu>
2020-07-06 14:09:45 -0400

    Remove legacy test trait

--- app/Controllers/Account.php
+++ app/Controllers/Account.php
@@ -1,25 +1,10 @@
 <?php namespace App\Controllers;
 
-use Tatter\Workflows\Models\JobModel;
-use CodeIgniter\Exceptions\PageNotFoundException;
-
 class Account extends BaseController
 {
-	use \Tests\Support\Traits\FakerTrait;
-
 	// Displays a list of jobs for the current user
 	public function jobs()
 	{
 		return view('account/jobs', ['jobs' => user()->jobs]);
-	}
-
-	public function generate()
-	{
-		$this->db = db_connect();
-
-		$this->fakerSetUp();
-		$this->fullFake();
-
-		return redirect()->to(base_url())->with('message', 'Faked data generated');
 	}
 }
