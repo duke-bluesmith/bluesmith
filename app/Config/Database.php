71e7d2e584b5a214ab98b913792e3461a642e780
Matt Gatner <matt.gatner@duke.edu>
2020-07-09 11:04:04 -0400

    Restore legacy database stub

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -79,6 +79,33 @@ class Database extends \CodeIgniter\Database\Config
 		'port'     => 3306,
 	];
 
+	/**
+	 * The legacy database connection (only needed if using Bluesmith\Upgrader).
+	 *
+	 * @var array
+	 */
+	public $legacy = [
+		'DSN'      => '',
+		'hostname' => 'localhost',
+		'username' => '',
+		'password' => '',
+		'database' => '',
+		'DBDriver' => 'MySQLi',
+		'DBPrefix' => '',
+		'pConnect' => false,
+		'DBDebug'  => (ENVIRONMENT !== 'production'),
+		'cacheOn'  => false,
+		'cacheDir' => '',
+		'charset'  => 'utf8',
+		'DBCollat' => 'utf8_general_ci',
+		'swapPre'  => '',
+		'encrypt'  => false,
+		'compress' => false,
+		'strictOn' => false,
+		'failover' => [],
+		'port'     => 3306,
+	];
+
 	//--------------------------------------------------------------------
 
 	public function __construct()
