8ce53d06e530e81447e369b01501b8432d7ee0a1
Matt Gatner <matt.gatner@duke.edu>
2020-10-16 15:09:57 -0400

    Tweak AdminLTE

--- app/Views/layouts/manage.php
+++ app/Views/layouts/manage.php
@@ -82,7 +82,7 @@
 	<aside class="main-sidebar sidebar-dark-primary elevation-4">
 		<!-- Brand Logo -->
 		<a href="<?= site_url('manage') ?>" class="brand-link">
-			<img src="<?= base_url('assets/images/logo-transparent.png') ?>" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+			<img src="<?= service('settings')->orgLogo ?>" alt="Logo" class="brand-image img-circle elevation-3 bg-white" style="opacity: .8">
 			<span class="brand-text font-weight-light"><?= service('settings')->brandName ?></span>
 		</a>
 
