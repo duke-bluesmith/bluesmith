46122ac3810cb61a4cf3a8c3b274e775d4ccc6e6
Matt Gatner <matt.gatner@duke.edu>
2021-02-18 11:05:28 -0500

    Bugfix job row caching

--- app/Models/JobModel.php
+++ app/Models/JobModel.php
@@ -1,6 +1,7 @@
 <?php namespace App\Models;
 
 use App\Entities\Job;
+use CodeIgniter\I18n\Time;
 use Tatter\Workflows\Models\JobModel as BaseJobModel;
 
 /**
@@ -18,6 +19,10 @@ class JobModel extends BaseJobModel
 	protected $with		     = ['options'];
 	protected $returnType	 = Job::class;
 	protected $allowedFields = ['name', 'summary', 'workflow_id', 'stage_id', 'material_id'];
+
+	protected $afterInsert = ['clearCompiledRows'];
+	protected $afterUpdate = ['clearCompiledRows'];
+	protected $afterDelete = ['clearCompiledRows'];
 
 	/**
 	 * Permits
@@ -65,5 +70,92 @@ class JobModel extends BaseJobModel
 			'user_id'    => (int) $userId,
 			'created_at' => date('Y-m-d H:i:s'),
 		]);
+	}
+
+	/**
+	 * Removes cached Job rows.
+	 *
+	 * @return void
+	 */
+	public function clearCompiledRows(): void
+	{
+		cache()->delete('jobrows');
+	}
+
+	/**
+	 * Fetch or build the Job rows for browsing,
+	 * applying filters and sorting.
+	 *
+	 * @param callable|null $filter
+	 * @param string $sort
+	 * @param bool $ascending
+	 *
+	 * @return array[]
+	 */
+	public function getCompiledRows(callable $filter = null, string $sort = 'id', bool $ascending = true): array
+	{
+		if (! $rows = cache('jobrows'))
+		{
+			// Pull all the data
+			$result = $this->builder()
+				->select('jobs.*, users.id AS user_id, users.firstname, users.lastname, workflows.name AS workflow, actions.name AS action, actions.role')
+				->join('jobs_users', 'jobs.id = jobs_users.job_id', 'left')
+				->join('users', 'jobs_users.user_id = users.id')
+				->join('workflows', 'jobs.workflow_id = workflows.id')
+				->join('stages', 'jobs.stage_id = stages.id', 'left')
+				->join('actions', 'stages.action_id = actions.id')
+				->get()->getResultArray();
+
+			// Process into rows
+			$rows = [];
+			foreach ($result as $row)
+			{
+				// Only keep the first match (from multiple Users join)
+				if (isset($rows[$row['id']]))
+				{
+					continue;
+				}
+
+				$rows[$row['id']] = $row;
+			}
+
+			// Cache the rows
+			$rows = array_values($rows);
+			cache()->save('jobrows', $rows, HOUR);
+		}
+
+		// Filter the array with the callable, or `null` which removes empties
+		$rows = $filter ? array_filter($rows, $filter) : array_filter($rows);
+
+		// Short circuit for unsortable results
+		if (count($rows) < 2)
+		{
+			return $rows;
+		}
+
+		// Convert timestamps to Time
+		$rows = array_map(function ($row) {
+			$row['created_at'] = new Time($row['created_at']);
+			$row['updated_at'] = new Time($row['updated_at']);
+
+			if (isset($row['deleted_at']))
+			{
+				$row['deleted_at'] = new Time($row['deleted_at']);
+			}
+
+			return $row;
+		}, $rows);
+
+		// Check for a valid sort request
+		if (array_key_exists($sort, reset($rows)))
+		{
+			usort($rows, function ($row1, $row2) use ($sort, $ascending) {
+				return $ascending
+					? $row1[$sort] <=> $row2[$sort]
+					: $row2[$sort] <=> $row1[$sort];
+			});
+		}
+
+		return $rows;
 	}
 }
