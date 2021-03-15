e52b84ff65a9a312765d5de0c6d8d5861e6c7bea
Matt Gatner <matt.gatner@duke.edu>
2021-03-15 12:33:39 -0400

    Reorganize tests

--- tests/misc/CommonTest.php
+++ tests/misc/CommonTest.php
@@ -11,18 +11,20 @@ class CommonTest extends ProjectTestCase
 	use \CodeIgniter\Test\DatabaseTestTrait;
 	use \Tests\Support\AuthenticationTrait;
 
+	public function testUserReturnsAppEntity()
+	{
+		$result = user();
+
+		$this->assertNotNull($result);
+		$this->assertEquals(User::class, get_class($result));
+	}
+
 	public function testUserReturnsNull()
 	{
+		$this->resetAuthServices();
+
 		$result = user();
 
 		$this->assertNull($result);
-	}
-
-	public function testUserReturnsAppUser()
-	{
-		$this->createAuthUser();
-		$result = user();
-
-		$this->assertEquals(User::class, get_class($result));
 	}
 }
